ba9e350cb5096c05756c229485ee6fa8e50e5111
attapack <75312811+attapack@users.noreply.github.com>
2021-09-13 15:11:17 +0300

    Create mx_strchr.c

---
+++ t02/mx_strchr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char* mx_strchr(const char*s, int c) { 
+    while(*s != c && *s != '\0') {
+        s++; 
+    } 
+    if (*s == c) {
+        return (char *)s; 
+    }
+    else return NULL;
+}
